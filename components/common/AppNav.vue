25cf00e7fdecfd07cdc77fe20da286930c874951
Christian <chrs.albert@gmail.com>
2020-07-24 15:38:37 +0200

    removes unused css

--- components/common/AppNav.vue
+++ components/common/AppNav.vue
@@ -33,34 +33,3 @@ export default {
   }
 }
 </script>
-<style scoped>
-  .span {
-    font-size: .8rem;
-    font-weight: 600;
-    margin-right: 8px;
-  }
-  .controls {
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    display: flex;
-    align-items: center;
-  }
-  label {
-    font-weight: bold;
-    font-size: .8rem
-  }
-  .head {
-    display: flex;
-    align-items: center
-  }
-  #settings {
-    position: fixed;
-    top: 4rem;
-    right: 0;
-    bottom: 0;
-    background: #000;
-    width: 40rem;
-    padding: 32px;
-  }
-</style>
