ad427b952902ad1627c87eae45ae871daab0139a
Christian <chrs.albert@gmail.com>
2020-08-03 20:18:34 +0200

    mobile nav

--- layouts/player.vue
+++ layouts/player.vue
@@ -1,7 +1,12 @@
 <template>
 	<app-layout modifier="player">
 		<Nuxt />
-		<template v-slot:header>
+		<template v-slot:header v-if="$device.isDesktop">
+			<app-header>
+				<player-nav />
+			</app-header>
+		</template>
+		<template v-slot:footer v-else>
 			<app-header>
 				<player-nav />
 			</app-header>
